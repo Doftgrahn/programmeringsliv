328547d5aa4653581cf7ce6cb7219ff400be7d9f
Simon grahn <simon.grahn@gmail.com>
2019-05-20 10:02:40 +0200

    jkj

--- src/Programmeringliv.js
+++ src/Programmeringliv.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./styles/App.scss";
 
-const App = () => {
+const Programmerlingsliv = () => {
     return (
         <div className="App">
             <h1>Hej</h1>
@@ -9,4 +9,4 @@ const App = () => {
     );
 };
 
-export default App;
+export default Programmerlingsliv;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-import App from './App';
+import Programmerlingsliv from './Programmeringliv';
 import * as serviceWorker from './tdd/serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Programmerlingsliv />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
