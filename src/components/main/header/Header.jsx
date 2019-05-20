0bbb22758d73835d69f7af65f271ed816c2f8d61
Simon grahn <simon.grahn@gmail.com>
2019-05-20 12:53:55 +0200

    changes before merge

--- src/components/main/header/Header.jsx
+++ src/components/main/header/Header.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 import NavLinks from "./NavLinks";
 
 const Header = () => {
+    //const [toggle, setToggle] = useState(false);
+
     return (
         <header>
             <NavLinks />
