a9c0ccb87b4ade8fac3224904f42060158bd7928
Simon grahn <simon.grahn@gmail.com>
2019-05-20 09:03:25 +0200

    first push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/App.scss";
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Hej</h1>
+        </div>
+    );
+};
+
+export default App;
