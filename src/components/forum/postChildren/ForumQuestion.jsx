fac932205bd263e41a4d5c86a275744dde9ed849
Simon grahn <simon.grahn@gmail.com>
2019-05-24 15:59:42 +0200

    changes to mobile

--- src/components/forum/postChildren/ForumQuestion.jsx
+++ src/components/forum/postChildren/ForumQuestion.jsx
@@ -3,8 +3,11 @@ import React from "react";
 const ForumQuestion = ({username, title, content, votes, picture}) => {
     return (
         <div className="post_container-question">
+            <p className="votes">Votes: {votes}</p>
+
             <h3 className="title">{title}</h3>
             <p className="content_c">{content}</p>
+
             <div className="postPicture-container">
                 {picture ? (
                     <img
@@ -14,7 +17,6 @@ const ForumQuestion = ({username, title, content, votes, picture}) => {
                     />
                 ) : null}
             </div>
-            <p className="votes">Votes: {votes}</p>
         </div>
     );
 };
