398c5d49cfc9ea751a7791ae92697a7b5ed2da0c
Simon grahn <simon.grahn@gmail.com>
2019-05-24 15:00:38 +0200

    small stuff forum

--- src/components/forum/postChildren/ForumQuestion.jsx
+++ src/components/forum/postChildren/ForumQuestion.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-const ForumQuestion = ({username, title, content, votes,picture}) => {
-    console.log(picture);
+const ForumQuestion = ({username, title, content, votes, picture}) => {
     return (
         <div className="post_container-question">
             <h3 className="title">{title}</h3>
             <p className="content">{content}</p>
             <p className="votes">Votes: {votes}</p>
-            {picture ? <img className="postPicture" src={picture} alt="postPicture" /> : null}
+            {picture ? (
+                <img className="postPicture" src={picture} alt="postPicture" />
+            ) : null}
         </div>
     );
 };
