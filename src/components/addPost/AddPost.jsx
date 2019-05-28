4b000115bfe58dc27dc33ab240f4f36d8f5ee22c
Olga <tselyuk.olga@gmail.com>
2019-05-28 20:01:11 +0200

    delete feature for the image

--- src/components/addPost/AddPost.jsx
+++ src/components/addPost/AddPost.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import FormPost from "./FormPost"
+import Writer from "./PostWriter"
 
 const AddPostPage = ({user}) => {
 
   return (
-    <div>
-    <FormPost user={user}/>
+    <div className="wrapper">
+      <h1>Add post</h1>
+      <div className="writerProfile">
+        <Writer user={user}/>
+      </div>
+      <div className="mainForm">
+      <FormPost user={user}/>
+      </div>
     </div>
   )
 
---
+++ src/components/addPost/PostWriter.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const Writer = ({user}) => {
+  let userId;
+  if(user) {
+    userId = user.uid;
+  }
+
+  return (
+    <div className="profileInfo">
+      <img src={!user ? "https://cdn.impactinit.com/cdn/x/x@ac8c3fd87c/smss53/smsimg28/pv/ingimagecontributors/ing_47129_07704.jpg" : user.photoURL}
+      alt="avatarImg"
+      className="avatarImg"
+      />
+      <div className="name">{!user ? 'User Unknown' : user.displayName}</div>
+      <div className="email">{!user ? 'email@gmail.com' : user.email}</div>
+    </div>
+  )
+}
+
+export default Writer;
