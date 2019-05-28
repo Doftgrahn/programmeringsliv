520e4e89cc90785b0cafb5ced53f7815b4210731
Simon grahn <simon.grahn@gmail.com>
2019-05-28 13:17:30 +0200

    vote system in progress

--- src/components/forum/Post.jsx
+++ src/components/forum/Post.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 
 import UserInfo from "./postChildren/UserInfo";
 import ForumQuestion from "./postChildren/ForumQuestion";
@@ -6,28 +6,17 @@ import ShowAnswer from "./postChildren/ShowAnswers";
 import AnswerQuestion from "./postChildren/AnswerQuestion";
 
 const Post = ({user, forumData, answers}) => {
-    const [isAnswering, setIsAnswering] = useState(false);
-
-    const showAnswerInput = () => {
-        setIsAnswering(true);
-    };
-    const hideAnswerInput = () => {
-        setIsAnswering(false);
-    };
-
     return (
         <article className="post">
             <div className="post_container">
                 <UserInfo forumData={forumData} />
                 <ForumQuestion forumData={forumData} user={user} />
-                <ShowAnswer user={user} forumQuestion={forumData} answers={answers} />
-                <AnswerQuestion
+                <ShowAnswer
                     user={user}
-                    isAnswering={isAnswering}
-                    showAnswerInput={showAnswerInput}
-                    hideAnswerInput={hideAnswerInput}
-                    forumData={forumData}
+                    forumQuestion={forumData}
+                    answers={answers}
                 />
+                <AnswerQuestion user={user} forumData={forumData} />
             </div>
         </article>
     );
