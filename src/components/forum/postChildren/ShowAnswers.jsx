1c608b53b8bd5629e858cbaf0160ac360906d074
Simon grahn <simon.grahn@gmail.com>
2019-05-30 13:35:01 +0200

    can not upvote and dovnvote comments

--- src/components/forum/postChildren/ShowAnswers.jsx
+++ src/components/forum/postChildren/ShowAnswers.jsx
@@ -6,7 +6,7 @@ import {database} from "../../../shared/Firebase";
 import collection from "../../../shared/dbCollection";
 
 const ShowAnswers = ({forumQuestion, user}) => {
-    const [answers, setAnswer] = useState(null);
+    const [answers, setAnswer] = useState([]);
 
     useEffect(() => {
         let isSubscribed = true;
@@ -23,68 +23,18 @@ const ShowAnswers = ({forumQuestion, user}) => {
         return () => (isSubscribed = false);
     }, []);
 
-    let answer;
-    if (answers)
-        answer = answers
-            .filter(answer => answer.postIdRef === forumQuestion.postiD)
-            .map((an, i) => (
-                <Answer
-                    key={an.id}
-                    answer={an}
-                    forumQuestion={forumQuestion}
-                    user={user}
-                />
-            ));
+    let answer = answers
+        .filter(answer => answer.postIdRef === forumQuestion.postiD)
+        .map(an => (
+            <Answer
+                key={an.id}
+                answer={an}
+                forumQuestion={forumQuestion}
+                user={user}
+            />
+        ));
 
-    return <>{!answers ? null : answer}</>;
+    return <div>{answers.length === 0 ? null : answer}</div>;
 };
 
 export default ShowAnswers;
-
-//forumQuestion.postID
-
-/*
-<div className="answer">
-    {filterAnswers.map(a => {
-        let date = a.timestamp.toDate().toLocaleDateString();
-        let time = a.timestamp.toDate().toLocaleTimeString();
-        return (
-            <div className="answer-container" key={a.id}>
-                <div className="answer-photo">
-                    <img src={a.photoURL} alt="userPhoto" />
-                </div>
-                <span>Username: {a.username}</span>
-                <span>
-                    posted:
-                    {date} {time}
-                </span>
-                <div className="vote">
-                    <img
-                        onClick={() => upVote(a)}
-                        className="upvote"
-                        src={voteArrow}
-                        alt="upvote"
-                    />
-                    <span>votes: {filteredUpvotes}</span>
-                    <img src={voteArrow} alt="downVote" />
-                </div>
-
-                <div className="answerAndDeletel-container">
-                    <p>
-                        Answer:
-                        {a.answer}
-                    </p>
-                    {user && a.userId === user.uid ? (
-                        <button
-                            className="deleteButton"
-                            onClick={() => deleteAnswer(a)}
-                        >
-                            delete
-                        </button>
-                    ) : null}
-                </div>
-            </div>
-        );
-    })}
-</div>
-*/
