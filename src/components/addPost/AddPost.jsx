657b14ad4f66ff9c4ffd1ef0d151e7b0b1d7ba28
Olga <tselyuk.olga@gmail.com>
2019-06-02 15:13:34 +0200

    user has to be logged in to write a post

--- src/components/addPost/AddPost.jsx
+++ src/components/addPost/AddPost.jsx
@@ -5,17 +5,25 @@ import LastPosts from "./LastPosts"
 
 const AddPostPage = ({user}) => {
 
+  if(!user){
+    return(
+      <div>Please, log in to write a post</div>
+    )
+  }
+  
   return (
-    <div className="wrapper">
+    <div className="wrapEverything wrapper">
       <h2>Add post</h2>
-      <div className="writerProfile">
+      <div className="mainGrid">
+        <div className="writerProfile">
         <Writer user={user}/>
-      </div>
-      <div className="mainForm">
-      <FormPost user={user}/>
-      </div>
-      <div className="lastPosts">
-      <LastPosts/>
+        </div>
+        <div className="mainForm">
+        <FormPost user={user}/>
+        </div>
+        <div className="lastPosts">
+        <LastPosts/>
+        </div>
       </div>
     </div>
   )
