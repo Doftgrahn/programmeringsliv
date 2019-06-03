79b3690ed03304b3142a51e1a2d2e9f722e7f217
Olga <tselyuk.olga@gmail.com>
2019-06-03 11:19:56 +0200

    added a picture

--- src/components/addPost/AddPost.jsx
+++ src/components/addPost/AddPost.jsx
@@ -8,7 +8,12 @@ const AddPostPage = ({user}) => {
   if(!user){
     return(
       <div id="notLoggedIn">
-      <h2>Please, log in to write a post</h2>
+      <h2>Please, log in to write a post!</h2>
+      <img
+        src="https://www.invennt.com/wp-content/uploads/2015/10/computer-says-no.jpg"
+        alt="computerSaysNo"
+        className="computerSaysNo"
+        />
       </div>
     )
   }
